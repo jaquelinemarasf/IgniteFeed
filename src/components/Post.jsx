51a2b36ccb985aa89e67f02a150be91b4db4037f
Jaqueline <jaquelinemarasf@gmail.com>
2024-09-18 20:05:45 -0300

    Estuturando Post

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,9 +4,31 @@ export function Post() {
     return (
     <article className={styles.post}>
         <header>
-            
+            <div className={styles.author}>
+                <img className={styles.avatar} src="https://github.com/jaquelinemarasf.png" />
+                <div className={styles.authorInfo}>
+                <strong>Jaqueline Mara</strong>
+                <span>Web Developer</span>
+                </div>
+            </div>
+        
+        <time title='18 de setembro às 17:02h' dateTime='18/09/2024 17:02:25'>Publicado á 1h</time>
         </header>
 
+     <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀</p>
+        <p>👉 <a href=''>jane.design/doctorcare</a></p>
+        <p><a href=''>#novoprojeto</a> <a href=''>#nlw</a> <a href=''>#rocketseat</a></p>
+    </div>
+
+    <form className={styles.commentForm}>
+        <strong>Deixe seu feedback</strong>
+        <textarea placeholder='Deixe um comentário...'/>
+        <footer>
+            <button type="submit">Publicar</button>
+        </footer>
+    </form>
     </article>
 
     )
